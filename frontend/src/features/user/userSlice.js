0e7226991575325044252f376782eb668900025b
JiHyoung You <fbwlgud51@gmail.com>
2025-08-02 10:04:52 +0900

    feat: 회원가입 registeruser thunk 추가 및 성공/실패 처리

--- frontend/src/features/user/userSlice.js
+++ frontend/src/features/user/userSlice.js
@@ -20,7 +20,30 @@ export const registerUser = createAsyncThunk(
   async (
     { email, name, password, navigate },
     { dispatch, rejectWithValue }
-  ) => {}
+  ) => {
+    try {
+      const response = await api.post("/user", { email, password, name });
+
+      dispatch(
+        showToastMessage({
+          message: "회원가입에 성공했습니다!",
+          status: "success",
+        })
+      );
+
+      navigate("/login");
+
+      return response.data.data;
+    } catch (err) {
+      dispatch(
+        showToastMessage({
+          message: "회원가입에 실패했습니다.",
+          status: "error",
+        })
+      );
+      return rejectWithValue(err.message);
+    }
+  }
 );
 
 export const loginWithToken = createAsyncThunk(
@@ -43,7 +66,20 @@ const userSlice = createSlice({
       state.registrationError = null;
     },
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(registerUser.fulfilled, (state) => {
+        state.loading = false;
+        state.registrationError = null;
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.loading = false;
+        state.registrationError = action.payload;
+      });
+  },
 });
 export const { clearErrors } = userSlice.actions;
 export default userSlice.reducer;
